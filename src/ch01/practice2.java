cb94c5b977ecdb0c159555346e7022221687102d
crystalS2 <kelly240926@gmail.com>
2023-11-16 09:54:29 +0900

    initialize kelly-server

---
+++ src/ch01/practice2.java
@@ -0,0 +1,22 @@
+package ch01;
+
+/**
+ * @author kelly
+ */
+/*
+장제목 : 1장 자바 시작하기
+작성일 : 2022.08.24
+ */
+public class practice2 {
+    //프로그램 실행 진입점
+    public static void main(String[] args) {
+        //콘솔에 출력하는 실행문
+        System.out.println("Hello, Java");
+
+        int x;
+        x = 1;
+        int y = 2;
+        int result = x + y;
+        System.out.println(result);
+    }
+}
---
+++ src/ch01/practice3.java
@@ -0,0 +1,11 @@
+package ch01;
+
+public class practice3 {
+    public static void main(String[] args){
+        int x = 1;
+        int y = 2;
+        int result = x + y;
+
+        System.out.println(result);
+    }
+}
